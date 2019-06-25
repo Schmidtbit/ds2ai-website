0eb867391581ff1cd7193baa10e46a0632c3167c
Andrea Schmidt <schmidtabox@gmail.com>
2019-06-25 14:51:51 -0500

    Updates

---
+++ precache-manifest.cccc57ee223794137fd70f695b53a2d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "528deab5535fe18668ece97814818afe",
+    "url": "/Schmidtbit/ds2ai-website.git/index.html"
+  },
+  {
+    "revision": "eb59ef6e378dc900bbd1",
+    "url": "/Schmidtbit/ds2ai-website.git/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Schmidtbit/ds2ai-website.git/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "eb59ef6e378dc900bbd1",
+    "url": "/Schmidtbit/ds2ai-website.git/static/js/main.04281923.chunk.js"
+  },
+  {
+    "revision": "f63e7fd9267644bb4257",
+    "url": "/Schmidtbit/ds2ai-website.git/static/js/runtime~main.300cb124.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Schmidtbit/ds2ai-website.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
